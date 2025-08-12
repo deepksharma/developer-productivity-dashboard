f9160996c264a3aef5f67bd563ce6546712cbad9
Deepak Prasad <official.deepakprasad@gmail.com>
2025-08-12 20:42:37 +0530

    Initial setup

---
+++ src/main/java/com/Deepak/DevProductivityTracker/DevProductivityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.Deepak.DevProductivityTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevProductivityTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevProductivityTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Deepak/DevProductivityTracker/DevProductivityTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Deepak.DevProductivityTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProductivityTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
